ac7b8ffa7498ec996e0f900d1fa74867dbf58e9a
vishal kumar[C <vishalkr0012@gmail.com>
2023-09-08 23:30:43 +0530

    helloVishal

---
+++ check.js
@@ -0,0 +1,16 @@
+let array=[1,2,3,4,5,6,7,8,9,0]
+let mess=[52,11,20,10,17,"shriram" ,1];
+if(mess.length>5){
+    if(mess.includes(100)){
+        console.log("shriram")
+    }
+        else{
+            console.log("faild")
+        }
+        console.log("vishal")
+    }
+    else{
+    console.log("erite");
+}
+
+
